4c0d8067d2eff8727da07efaa192cc0ea920214f
Gbriel Chabot <gabriel.chabot@epita.fr>
2020-09-25 21:33:16 +0200

    [COMMON] Add AAAA record type

--- src/common/common.h
+++ src/common/common.h
@@ -1,12 +1,14 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <stdint.h>
+
 #define TYPE_A      1
 #define TYPE_NS     2
 #define TYPE_CNAME  5
 #define TYPE_SOA    6
-#define TYPE_PTR    12
 #define TYPE_MX     15
+#define TYPE_AAAA   28
 
 #endif
 
