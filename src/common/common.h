bd6dabbcbe66ef39a5e6ed3199ca0a1ee7acf464
Gbriel Chabot <gabriel.chabot@epita.fr>
2020-09-25 21:49:03 +0200

    [ADD] Add DNS header struct

--- src/common/common.h
+++ src/common/common.h
@@ -3,12 +3,30 @@
 
 #include <stdint.h>
 
+
 #define TYPE_A      1
 #define TYPE_NS     2
 #define TYPE_CNAME  5
 #define TYPE_SOA    6
 #define TYPE_MX     15
 #define TYPE_AAAA   28
+
+struct header
+{
+    uint16_t    id;
+    uint8_t     qr :1;
+    uint8_t     opcode :4;
+    uint8_t     aa :1;
+    uint8_t     tc :1;
+    uint8_t     rd :1;
+    uint8_t     ra :1;
+    uint8_t     z :3;
+    uint8_t     rcode :4;
+    uint16_t    qdcount;
+    uint16_t    ancount;
+    uint16_t    nscount;
+    uint16_t    arcount;
+} packed
 
 #endif
 
