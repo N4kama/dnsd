4cc29e6f92263a877414879d0f96eb779a24f71f
Gbriel Chabot <gabriel.chabot@epita.fr>
2020-09-26 20:42:46 +0200

    [ADD] A unit test for parsing (need to improve)

--- src/unit_tests/unit.c
+++ src/unit_tests/unit.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "error.h"
 #include "option_parser.h"
@@ -9,6 +10,7 @@
 #include "colors.h"
 
 void test_display_header(void);
+void test_parse_header(void);
 
 int main(int argc, char *argv[])
 {
@@ -18,6 +20,7 @@ int main(int argc, char *argv[])
     printf("DNSD TEST SUITE START\n");
 
     test_display_header();
+    test_parse_header();
 
 }
 
@@ -25,4 +28,15 @@ void test_display_header(void)
 {
     header h;
     display_header(&h);
+}
+
+void test_parse_header(void)
+{
+    FILE *packet = fopen("samples/dnsquery.raw", "r");
+    char *buf = calloc(32, sizeof(char));
+    fread(buf,sizeof(char), 32, packet);
+    message *m = malloc(sizeof(message));
+    parse_message(buf, m);
+    display_header(&(m->header));
+    display_question(m->question);
 }
