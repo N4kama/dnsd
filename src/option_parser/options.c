a577086abfaa81d871cec93278955d23f975f3a9
Raymond Haddad <raymond.haddad@epita.fr>
2020-09-26 21:12:05 +0200

    [FIX] Init option no more needed

--- src/option_parser/options.c
+++ src/option_parser/options.c
@@ -7,112 +7,60 @@
 
 #include "options.h"
 
-static opt *g_opt = NULL;
-
-static void init_opt(void)
-{
-    if (g_opt != NULL)
-        return;
-   g_opt = calloc(1, sizeof(opt));
-   g_opt->file = NULL;
-   //FIX : Define values : if -f ... and no -v4 there is no IPv4 by default but if you give 0 options there is
-}
+static opt g_opt = {
+    .v4 = { .s_addr = 0 },
+    .v6 = { 0 },
+    .p_udp = 53,
+    .p_tcp = 53,
+    .file = NULL
+};
 
 void set_file(char *path)
 {
-    init_opt();
-    if (g_opt != NULL && g_opt->file == NULL)
-    {
-        g_opt->file = malloc(strlen(path) + 1);
-        strcpy(g_opt->file, path);
-    }
-
-    //TODO : Redefine path ?
+    g_opt.file = path;
 }
 
 char *get_file(void)
 {
-    if (g_opt->file != NULL)
-        return g_opt->file;
-    return NULL;
+    return g_opt.file;
 }
 
 void set_v4(struct in_addr ip_v4)
 {
-    init_opt();
-    if (g_opt != NULL)
-        g_opt->v4 = ip_v4;
-    return;
+    g_opt.v4 = ip_v4;
 }
 
 struct in_addr get_v4(void)
 {
-    if (g_opt != NULL)
-        return g_opt->v4;
-
-    struct in_addr inaddr;
-    inet_pton(AF_INET, "127.0.0.1", &inaddr);
-    return inaddr;
+    return g_opt.v4;
 }
 
 void set_v6(struct in6_addr ip_v6)
 {
-    init_opt();
-    if (g_opt != NULL)
-       g_opt->v6 = ip_v6;
-    return;
+    g_opt.v6 = ip_v6;
 }
 
 struct in6_addr get_v6(void)
 {
-    if (g_opt != NULL)
-        return g_opt->v6;
-
-    struct in6_addr in6addr;
-    inet_pton(AF_INET6, "::1", &in6addr);
-    return in6addr;
+    return g_opt.v6;
 }
 
 void set_port_udp(char *p_udp)
 {
-    init_opt();
-    if (g_opt != NULL)
-    {    
-        g_opt->p_udp = atoi(p_udp); //FIX ME: May break
-        return;
-    }
-    g_opt->p_udp = 53;
+    g_opt.p_udp = atoi(p_udp);
 }
 
 uint16_t get_port_udp(void)
 {
-    if (g_opt != NULL)
-    {
-        if (g_opt->p_udp == 0)
-            return 53;
-        return g_opt->p_udp;
-    }
-    return 53;
+    return g_opt.p_udp;
 }
 
 void set_port_tcp(char *p_tcp)
 {
-    init_opt();
-    if (g_opt != NULL)
-    {
-        g_opt->p_tcp = atoi(p_tcp); //FIX ME: May break
-        return;
-    }
-    g_opt->p_tcp = 53;
+    g_opt.p_tcp = atoi(p_tcp);
 }
 
 uint16_t get_port_tcp(void)
 {
-    if (g_opt != NULL)
-    {
-        if (g_opt->p_tcp == 0)
-            return 53;
-        return g_opt->p_tcp;
-    }
-    return 53;
+    return g_opt.p_tcp;
 }
