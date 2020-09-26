7cc5fc32daf18f27d5c68d882319458d6da132ab
Raymond Haddad <raymond.haddad@epita.fr>
2020-09-26 19:44:13 +0200

    [MAIN] Adding the option parser

--- src/main.c
+++ src/main.c
@@ -3,10 +3,19 @@
 #include "error.h"
 #include "zone_file_parser.h"
 #include "server.h"
+#include "option_parser.h"
+#include "options.h"
 
-int main(void) // int argc, char *argv[]) 
+int main(int argc, char **argv)
 {
     dnsd_err code;
+
+    if (argc >= 2)
+    {
+        code = parse_options(argc, argv);
+        if (code != ERR_OK)
+            return code;
+    }
 
     zone_array *zones;
     code = zone_parse("samples/dnsd.zone", &zones);
