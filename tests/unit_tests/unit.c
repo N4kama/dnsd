8fd55d63ca313c6aefeafac05710f04782b7828e
Gbriel Chabot <gabriel.chabot@epita.fr>
2020-09-26 22:09:44 +0200

    [ADD] Add test for parsing and creating raw packet

--- tests/unit_tests/unit.c
+++ tests/unit_tests/unit.c
@@ -11,6 +11,7 @@
 
 void test_display_header(void);
 void test_parse_header(void);
+void test_parse_and_write(void);
 
 int main(int argc, char *argv[])
 {
@@ -21,6 +22,7 @@ int main(int argc, char *argv[])
 
     test_display_header();
     test_parse_header();
+    test_parse_and_write();
 
 }
 
@@ -39,4 +41,34 @@ void test_parse_header(void)
     parse_message(buf, m);
     display_header(&(m->header));
     display_question(m->question);
+}
+
+void test_parse_and_write(void)
+{
+    puts("\n ---BEGIN test_parse_and_write--- \n");
+    FILE *packet = fopen("samples/dnsquery.raw", "r");
+    char *buf = calloc(32, sizeof(char));
+    fread(buf,sizeof(char), 32, packet);
+    message *m = malloc(sizeof(message));
+    parse_message(buf, m);
+    display_header(&(m->header));
+    display_question(m->question);
+
+    char *result;
+    size_t rsize = message_to_raw(*m, &result);
+    if (rsize != 32)
+    {
+        printf("rsize is %lu instead of 32\n", rsize);
+        return;
+    }
+    if (memcmp(buf, result, 32))
+    {
+        puts("result data did not match input data");
+        for (size_t i = 0; i < 32; i++)
+        {
+            printf("0x%x     0x%x\n", buf[i], result[i]);
+        }
+        return;
+    }
+    puts("test_parse_and_write OK");
 }
