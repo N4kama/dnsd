df9b0782da8473fe4dbba275765e5a0af2d0811b
Céline WU <celine.wu@epita.fr>
2020-09-26 16:34:45 +0200

    [RESPONSE] started handler

--- include/response_handler.h
+++ include/response_handler.h
@@ -1,5 +1,12 @@
 #ifndef REPONSE_HANDLER_H
 #define REPONSE_HANDLER_H
+
+#include "common.h"
+#include "zone_file_parser.h"
+
+message *response_handle(message **response, message *request, zone_array *zones);
+zone *response_lookup(question question, zone_array *zones);
+
 #endif
 
 // Functions used to respond to a request
