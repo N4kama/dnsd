1c5f5f19b47ffd23cb06beb2583b757471908f4a
William Lin <william.lin@epita.fr>
2020-09-26 02:23:49 +0200

    [OPT] fix empty file

--- include/option_parser.h
+++ include/option_parser.h
@@ -1,6 +1,15 @@
 #ifndef _OPTION_PARSER_H_
 #define _OPTION_PARSER_H_
 
+#include <stdint.h>
+
+typedef struct
+{
+    uint8_t v4;
+    uint8_t v6;
+    uint8_t port;
+    //FIXME
+} opt;
 
 
 #endif /* ! OPTION_PARSER_H */
